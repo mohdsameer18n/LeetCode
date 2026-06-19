239ce38c86a72a6b08f2fce59badd1fe49b54032
Mohd Sameer <mohdsameer18n04@gmail.com>
2026-06-19 22:08:48 +0530

    Implement method to calculate clock angle

---
+++ Angle Between Hands of a Clock/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+
+        double minHand = 6.0 * minutes;
+        double hourHand = 30.0 * (hour % 12) + 0.5 * minutes;
+
+        double diff = Math.abs(hourHand - minHand);
+
+        return Math.min(diff, 360 - diff);
+    }
+}
